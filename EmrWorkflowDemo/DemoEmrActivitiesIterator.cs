04abae0739e7210f15912d680227fb8684762552
supperslonic <natalia.a.zelenskaya@gmail.com>
2014-03-02 10:33:59 -0600

    Demo

---
+++ EmrWorkflowDemo/DemoEmrActivitiesIterator.cs
@@ -0,0 +1,73 @@
+﻿using EmrWorkflow.Model;
+using EmrWorkflow.Model.Serialization;
+using EmrWorkflow.Model.Steps;
+using EmrWorkflow.Run;
+using EmrWorkflow.Run.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace EmrWorkflowDemo
+{
+    public class DemoEmrActivitiesIterator : EmrActivitiesIterator
+    {
+        private IEnumerator<EmrActivityStrategy> currentFlow;
+        private IEnumerator<EmrActivityStrategy> successFlow;
+        private IEnumerator<EmrActivityStrategy> errorFlow;
+
+        public DemoEmrActivitiesIterator()
+        {
+            this.successFlow = new List<EmrActivityStrategy>()
+            {
+                this.CreateStartActivity(),
+                this.CreateAddStepsActivity(),
+                this.CreateTerminateActivity("Job succeeded. terminate cluster")
+            }.GetEnumerator();
+
+            this.errorFlow = new List<EmrActivityStrategy>() 
+            {
+                this.CreateTerminateActivity("Terminate job because of error")
+            }.GetEnumerator();
+
+            this.currentFlow = this.successFlow;
+        }
+
+        public bool MoveNext
+        {
+            get { return this.currentFlow.MoveNext(); }
+        }
+
+        public EmrActivityStrategy Current
+        {
+            get { return this.currentFlow.Current; }
+        }
+
+        public void NotifyJobFailed()
+        {
+            this.currentFlow = this.errorFlow;
+        }
+
+        private EmrActivityStrategy CreateStartActivity()
+        {
+            XmlDocument jobFlowXml = new XmlDocument();
+            jobFlowXml.Load("Workflow/JobConfig.xml");
+            JobFlow jobFlow = JobFlow.GetRecord(jobFlowXml.OuterXml);
+
+            return new StartJobStrategy("start and configure job", jobFlow);
+        }
+
+        private EmrActivityStrategy CreateAddStepsActivity()
+        {
+            XmlDocument stepsXml = new XmlDocument();
+            stepsXml.Load("Workflow/Steps.xml");
+            IList<StepBase> steps = new StepsXmlFactory().ReadXml(stepsXml.OuterXml);
+
+            return new AddStepsStrategy("first activity", steps);
+        }
+
+        private EmrActivityStrategy CreateTerminateActivity(string name)
+        {
+            return new TerminateJobStrategy(name);
+        }
+    }
+}
---
+++ EmrWorkflowDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Amazon;
+using Amazon.ElasticMapReduce;
+using EmrWorkflow.RequestBuilders;
+using EmrWorkflow.Run;
+using System;
+using System.Threading;
+
+namespace EmrWorkflowDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuilderSettings settings = Program.CreateSettings();
+            AmazonElasticMapReduceClient emrClient = Program.CreateEmrClient();
+            DemoEmrActivitiesIterator activitiesIterator = new DemoEmrActivitiesIterator();
+
+            EmrJobRunner emrRunner = new EmrJobRunner(settings, emrClient, activitiesIterator);
+
+            //======== If you want to add steps to an existing job ========
+            //runner.JobFlowId = "j-111AAABBBNJ2I"; //explicitly set an existing jobFlowId
+            //runner.EmrActivities.RemoveAt(0); //remove start and configure job step
+            //=============================================================
+
+            emrRunner.Run();
+
+            while (emrRunner.IsRunning)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+
+        /// <summary>
+        /// Create settings to replace placeholders
+        /// </summary>
+        /// <returns>Settings</returns>
+        public static BuilderSettings CreateSettings()
+        {
+            return new BuilderSettings();
+        }
+
+        public static AmazonElasticMapReduceClient CreateEmrClient()
+        {
+            String accessKey = "";
+            String secretKey = "";
+            return new AmazonElasticMapReduceClient(accessKey, secretKey, RegionEndpoint.USEast1);
+        }
+    }
+}
